639840ae55bc660184b77d6b985dac459641adf1
Naoya Iwamoto <kazeneko1023@icloud.com>
2024-12-22 00:38:37 +0900

    [FIX]: layoutをしゅうせい　

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./providers";
 import { siteConfig } from "@/config/site";
 import { fontSans } from "@/config/fonts";
 import { Navbar } from "@/components/navbar";
+import { gsap } from "gsap";
 
 export const metadata: Metadata = {
   title: {
--- app/page.tsx
+++ app/page.tsx
@@ -1,50 +1,25 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
+"use client";
+import { useEffect, useRef } from "react";
 
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+const PinAnimation = () => {
+	const pinRef = useRef(null);
 
-export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "blue" })}>beautiful&nbsp;</span>
-        <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
-      </div>
+	
 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
+	return (
+		<section>
+			<div className="h-screen bg-blue w-full flex justify-center items-center">
+				<p className="text-[100px]">section 1</p>
+			</div>
+			<div ref={pinRef} className="h-screen w-full bg-dark flex items-center justify-center">
+				<p className="text-[100px]">section 2</p>
+			</div>
+			<div className="h-screen bg-green w-full flex justify-center items-center">
+				<p className="text-[100px]">section 3</p>
+			</div>
+		</section>
+	);
+};
 
-      
-    </section>
-  );
-}
+export default PinAnimation;
+
